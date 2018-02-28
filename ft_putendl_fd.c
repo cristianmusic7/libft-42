318d7137362a574b54571a7fab80810cb2aa62e0
Cristian Franco <cfranco@e1z3r5p18.42.us.org>
2018-02-27 17:31:37 -0800

    files restored

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -17,4 +17,13 @@ void	ft_putendl_fd(char const *str, int fd)
 	int i;
 
 	i = 0;
+	if (str)
+	{
+		while (str[i] != '\0')
+		{
+			ft_putchar_fd(str[i], fd);
+			i++;
+		}
+		ft_putchar_fd('\n', fd);
+	}
 }
